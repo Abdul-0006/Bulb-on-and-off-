6f3324bb32da59c0cd12390b30c3f813278bc672
Abdul-0006 <154364633+Abdul-0006@users.noreply.github.com>
2025-08-27 16:09:54 +0500

    Final commit

--- BULB SWITCH USING REACT/src/App.jsx
+++ BULB SWITCH USING REACT/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+// At the top of your App.jsx (with other imports)
+import bulbOn from './assets/bulbon.png';
+import bulbOff from './assets/bulboff.png';
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Bulb = () => {
+  const [State, setState] = useState(false)
+  const toggleBulb = () => {
+     setState(!State)
+  }
+  const imgState = () => {
+    if(State === true){
+      return bulbOff
+    }
+    else{
+      return bulbOn
+    }
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='main'>
+      <h1 className='title'>Bulb switch on and off </h1>
+      <img className='bulb-image' src= {imgState()} alt="Image not available" />
+      <button className='btn' onClick={toggleBulb}>{State ? "Turn on" : 'Turn off'} </button>
+    
+    </div>
   )
 }
 
-export default App
+export default Bulb
